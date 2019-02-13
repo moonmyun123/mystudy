6d87e15c4500402f3b615ba0713f5815bc47b550
moonm <moonm@211.218.248.195>
2019-02-13 16:40:23 +0900

    test

---
+++ src/main/java/net/study/MyStudyApplication.java
@@ -0,0 +1,14 @@
+package net.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyStudyApplication.class, args);
+	}
+
+}
+
