df5b3bd1506e1994a6b7b5efbd306ee860761f69
moonm <moonm@211.218.248.195>
2019-02-22 13:54:38 +0900

    test

--- src/main/java/net/study/MyStudyApplication.java
+++ src/main/java/net/study/MyStudyApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MyStudyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MyStudyApplication.class, args);
+		SpringApplication.run(MyStudyApplication.class, args); 
 	}
 
 }
